728c07d98810d504e17562f02bbb6c9d3f069ad7
tjwang <tengjingwang@gmail.com>
2020-04-14 13:33:30 +0800

    initial version for junit5

---
+++ src/main/java/me/tjwang/app/App.java
@@ -0,0 +1,14 @@
+package me.tjwang.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public int aVar = 1;
+}
